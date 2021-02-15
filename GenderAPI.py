93989402e1a2482c267981fff7d15f2ef28adba2
Marius Schulte <ge35fan@mytum.de>
2021-02-15 22:31:13 +0100

    Create GenderAPI.py

---
+++ GenderAPI.py
@@ -0,0 +1,52 @@
+import numpy as np
+import pandas as pd
+import requests
+import json
+from urllib.request import urlopen
+
+
+#Daten einlesen
+pd.options.display.max_columns = None
+
+people = pd.read_csv('/Users/schultemarius/Desktop/TUM/Techlabs/archive/people.csv', low_memory=False)
+relationships = pd.read_csv('/Users/schultemarius/Desktop/TUM/Techlabs/archive/relationships.csv', low_memory=False)
+people["Gender"] = ""
+
+# TECHAPI
+# https://ethnicolr.readthedocs.io/ethnicolr.html#installation
+# name
+# Alternative: https://gender-api.com/de/pricing
+# BIRTHPLACE!
+
+# unique Names List
+people["first_name"] = people["first_name"].str.replace("Dr.", "")
+people["first_name"] = people["first_name"].str.split("-").str[0]
+people["first_name"] = people["first_name"].str.split(" ").str[0]
+
+#.sort_values(by="first_name")
+firstNames_unique = people.drop_duplicates(subset="first_name",keep='last')[["first_name","Gender"]]
+firstNames_unique.to_csv('firstNames_unique.csv')
+
+
+firstNames_Gender = firstNames_unique.copy()
+firstNames_Gender.to_csv('firstNames_Gender.csv')
+firstNames_Gender.reset_index(drop=True, inplace=True)
+
+# ToDo
+# Sonderzeichen löschen?
+# Case Sensitive?
+
+# Gender API
+# https://genderapi.io/api-documentation
+
+apiKey = "6027c00f0c5ad00e625f1f12"
+
+# for i in range(len(firstNames_unique)):
+for i in range(5):
+ name = firstNames_unique.iloc[i]["first_name"]
+ apiUrl = "https://genderapi.io/api/?name=" + name + "&key=" + apiKey
+ result = urlopen(apiUrl).read().decode('utf-8')
+ getGender = json.loads(result)
+ firstNames_Gender.loc[[i], ["Gender"]] = getGender["gender"]
+
+print(firstNames_Gender)
