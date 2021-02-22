82deb9027d6a9a234cf12b5a60c6a49cd4722181
Marius Schulte <ge35fan@mytum.de>
2021-02-22 15:31:28 +0100

    Update

--- Gender_Race.py
+++ Gender_Race.py
@@ -18,35 +18,42 @@ people["Gender"] = ""
 # Alternative: https://gender-api.com/de/pricing
 # BIRTHPLACE!
 
-# unique Names List
+# Gender:
+# Gender API
+# https://genderapi.io/api-documentation
+
 people["first_name"] = people["first_name"].str.replace("Dr.", "")
 people["first_name"] = people["first_name"].str.split("-").str[0]
 people["first_name"] = people["first_name"].str.split(" ").str[0]
 
 #.sort_values(by="first_name")
 firstNames_unique = people.drop_duplicates(subset="first_name",keep='last')[["first_name","Gender"]]
-firstNames_unique.to_csv('firstNames_unique.csv')
-
+# firstNames_unique.to_csv('firstNames_unique.csv')
 
 firstNames_Gender = firstNames_unique.copy()
-firstNames_Gender.to_csv('firstNames_Gender.csv')
+# firstNames_Gender.to_csv('firstNames_Gender.csv')
 firstNames_Gender.reset_index(drop=True, inplace=True)
-
-# ToDo
-# Sonderzeichen löschen?
-# Case Sensitive?
-
-# Gender API
-# https://genderapi.io/api-documentation
 
 apiKey = "6027c00f0c5ad00e625f1f12"
 
 # for i in range(len(firstNames_unique)):
 for i in range(5):
- name = firstNames_unique.iloc[i]["first_name"]
- apiUrl = "https://genderapi.io/api/?name=" + name + "&key=" + apiKey
- result = urlopen(apiUrl).read().decode('utf-8')
- getGender = json.loads(result)
- firstNames_Gender.loc[[i], ["Gender"]] = getGender["gender"]
+  name = firstNames_unique.iloc[i]["first_name"]
+  apiUrl = "https://genderapi.io/api/?name=" + name + "&key=" + apiKey
+  result = urlopen(apiUrl).read().decode('utf-8')
+  getGender = json.loads(result)
+  firstNames_Gender.loc[[i], ["Gender"]] = getGender["gender"]
 
 print(firstNames_Gender)
+
+# Ethnicity
+
+lastNames_unique = people.drop_duplicates(subset="last_name",keep='last')[["last_name"]]
+# lastNames_unique.to_csv('lastNames_unique.csv')
+lastNames_Ethnicity = lastNames_unique.copy()
+lastNames_Ethnicity.reset_index(drop=True, inplace=True)
+
+pred_census_ln(lastNames_Ethnicity, "last_name")
+lastNames_Ethnicity.drop(columns=["api", "black", "hispanic","white"], inplace = True)
+
+print(lastNames_Ethnicity)
