7d005dbbdf478b6e54cf7290d00ca30357df7ae2
SebastianStohrer <79085704+SebastianStohrer@users.noreply.github.com>
2021-03-29 09:26:25 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -16,13 +16,6 @@ dfpeople = pd.DataFrame(people, columns = ['object_id', 'first_name', 'last_name
 #dfrelationships = pd.DataFrame(relationships, columns = ['person_object_id', 'relationship_object_id'])
 #dfobjects = pd.DataFrame(objects, columns = ['entity_id', 'name'])
 
-#Merge/Join tables -->error: int zu Object
-#dfshort.object_id.astype(str)
-#dfpeople.object_id.astype(str)
-#dfshort = dfshort.join(dfpeople, on = 'object_id')
-#print(dfshort.dtypes)
-#print(dfpeople.dtypes)
-
 #Part 1: subjects
 
 #Spalte pro Kategorie hinzufügen mit Anfangswert 0 (für Dummy Variablen):
@@ -36,18 +29,12 @@ dfshort['Medical and Health'] = 0
 dfshort['Business'] = 0
 dfshort['Others'] = 0
 
-#Column Data Type aller Spalten: Object -->convert to String
-#print(dfshort.dtypes)
-
-#convert column types to string
+#convert column types from object to string
 dfshort = dfshort.convert_dtypes()
 #print(dfshort.dtypes)
 
 def dummy(ausgangstabelle, schlagwort, kategorietabelle):
     dfshort.loc[dfshort[ausgangstabelle].str.contains(schlagwort), kategorietabelle] = 1
-
-#Data Cleaning by Replacing
-#dfshort.loc[dfshort['subject'].str.contains('Business | Finance'), 'subject'] = 'Business'
 
 #alle subjects in Kleinbuchstaben umwandeln
 dfshort['subject'] = dfshort['subject'].str.lower()
@@ -57,7 +44,7 @@ compScienceList = ['comput', 'software', 'informat', 'artifical', 'artificial', 
 for i in compScienceList:
     dummy('subject', i, 'Computer Science')
 
-# ebenso: cs = computer science -->ohne contains, sondern Gleichheit
+#ebenso: cs = computer science -->ohne contains, sondern Gleichheit
 dfshort.loc[(dfshort['subject'] == 'it'), 'Computer Science'] = 1
 dfshort.loc[(dfshort['subject'] == 'cs'), 'Computer Science'] = 1
 
@@ -115,14 +102,54 @@ dfshort.loc[(dfshort['Computer Science'] == 0) & (dfshort['Engineering'] == 0) &
 & (dfshort['Natural Sciences'] == 0) & (dfshort['Social Sciences'] == 0) & (dfshort['Humanities'] == 0)
 & (dfshort['Medical and Health'] == 0) & (dfshort['Business'] == 0), 'Others'] = 1
 
-#Part 2: still to do: degree titel + university
+#Merge/Join tables -->error: int zu Object
+#dfshort = dfshort.object_id.astype(str) #Ergebnis Abspeichern
+#dfpeople = dfpeople.object_id.astype(str)
+#dfshort = dfshort.join(dfpeople, on = 'object_id')
+#print(dfshort.dtypes)
+#print(dfpeople.dtypes)
 
+#Part 2: still to do: degree titel + university
+dfshort['degree_type'] = dfshort['degree_type'].str.lower()
+
+#Spalte pro Kategorie hinzufügen mit Anfangswert 0 (für Dummy Variablen):
+dfshort['Master'] = 0
+dfshort['Bachelor'] = 0
+dfshort['Phd'] = 0
+
+#Kategorie Master:
+masterList = ['mba', 'ms', 'master', 'jd', 'm.math', 'mps', 'mphil', 'mpa', 'meng', 'diplom',
+              'post', 'j.d', 'mph', 'm.b.a', 'm.e', 'dipl']
+for i in masterList:
+    dummy('degree_type', i, 'Master')
+dfshort.loc[(dfshort['degree_type'] == 'ma'), 'Master'] = 1
+dfshort.loc[(dfshort['degree_type'] == 'mis'), 'Master'] = 1
+
+#Kategorie Bachelor:
+bachelorList = ['bcs', 'llb', 'bfa', 'bba', 'bache', 'bacs', 'bs', 'bcom','bsc', 'b.s', 'bachlelors',
+                'b.business', 'b.engineering', 's.b', 'btech', 'beng', 'undergrad', 'scb', 'b.a.', 'bca','b.a' ]
+for i in bachelorList:
+    dummy('degree_type', i, 'Bachelor')
+# später: ee = electrical engineering -->ohne contains, sondern Gleichheit
+dfshort.loc[(dfshort['degree_type'] == 'be'), 'Bachelor'] = 1
+dfshort.loc[(dfshort['degree_type'] == 'ba'), 'Bachelor'] = 1
+dfshort.loc[(dfshort['degree_type'] == 'aa'), 'Bachelor'] = 1
+dfshort.loc[(dfshort['degree_type'] == 'sb'), 'Bachelor'] = 1
+dfshort.loc[(dfshort['degree_type'] == 'bs'), 'Bachelor'] = 1
+
+#Kategorie Phd:
+PhdList = ['phd', 'ph.d', 'dphil', 'md', 'doctor', 'dr']
+for i in PhdList:
+    dummy('degree_type', i, 'Phd')
+
+#Idee: falls 1 bei Phd, 0 bei Master und Bachelor
+#sowie falls 1 bei Master, dann 0 bei Bachelor
 
 #Ausgabe Zusammenfassung
 #print(dfshort.describe(include="all"))
 
-#transform to csv again (leider nur ersten 16.000 nicht alle 100.000)
-#dfshort.to_csv('/Users/Basti/Desktop/Test6.csv')
+#transform to csv again
+dfshort.to_csv('/Users/Basti/Desktop/Testdegree2.csv')
 
 
 
