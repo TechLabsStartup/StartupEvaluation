f1c21af19ce0c5663178f9d2561e0417aeb9e1d6
Marius Schulte <ge35fan@mytum.de>
2021-04-12 17:00:42 +0200

    HEllO WORLD

--- model_Test_JM.py
+++ model_Test_JM.py
@@ -310,7 +310,6 @@ def downsample_data(data):
 
     return df_downsampled
 
-
 data = prepare_data()
 data_downsampled = downsample_data(data)
 
@@ -320,7 +319,39 @@ data_downsampled = downsample_data(data)
 model_SVC(data_downsampled)
 
 
+
+
 #data.to_csv('data.csv')
+
+
+
+########################
+
+
+
+import streamlit as st
+
+st.sidebar.header('Diabetes Prediction')
+select = st.sidebar.selectbox('Select Form', ['Form 1'], key='1')
+if not st.sidebar.checkbox("Hide", True, key='1'):
+    st.title('Diabetes Prediction(Only for females above 21years of    Age)')
+    name = st.text_input("Name:")
+    pregnancy = st.number_input("No. of times pregnant:")
+    glucose = st.number_input("Plasma Glucose Concentration :")
+    bp =  st.number_input("Diastolic blood pressure (mm Hg):")
+    skin = st.number_input("Triceps skin fold thickness (mm):")
+    insulin = st.number_input("2-Hour serum insulin (mu U/ml):")
+    bmi = st.number_input("Body mass index (weight in kg/(height in m)^2):")
+    dpf = st.number_input("Diabetes Pedigree Function:")
+    age = st.number_input("Age:")
+submit = st.button('Predict')
+if submit:
+        prediction = classifier.predict([[pregnancy, glucose, bp, skin, insulin, bmi, dpf, age]])
+        if prediction == 0:
+            st.write('Congratulation',name,'You are not diabetic')
+        else:
+            st.write(name," we are really sorry to say but it seems like you are Diabetic.")
+```
 
 
 
